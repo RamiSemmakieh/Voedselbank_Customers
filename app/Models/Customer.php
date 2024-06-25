20205c8994dd862fcdaea5f6e439f14f8f2f5594
ramos013 <334020@student.mboutrecht.nl>
2024-06-25 10:46:41 +0200

    Add Customer model with attributes and relationships
    - Define Customer model with attributes: name, email, phone_number, adults, children, babies, street_name, house_number, postal_code, city
    - Add relationships: hasMany specialRequests, hasMany foodPackages

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone_number',
+        'adults',
+        'children',
+        'babies',
+        'street_name',
+        'house_number',
+        'postal_code',
+        'city',
+    ];
+
+    public function specialRequests()
+    {
+        return $this->hasMany(SpecialRequest::class);
+    }
+
+    public function foodPackages()
+    {
+        return $this->hasMany(FoodPackage::class);
+    }
 }
