6b900500a15341b71d11d21387adf7bcd4f93439
ramos013 <334020@student.mboutrecht.nl>
2024-06-25 13:11:57 +0200

    Add CustomerSeeder to seed customers table with sample data

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Customer;
 
 class CustomerSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Customer::factory()->count(5)->create();
     }
 }
