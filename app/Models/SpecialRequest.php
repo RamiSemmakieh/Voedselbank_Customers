7943f424ab946cfcb1a6a80cc85b63673fc33498
ramos013 <334020@student.mboutrecht.nl>
2024-06-25 10:47:50 +0200

    add specialRequest model with attributes and relationships

--- app/Models/SpecialRequest.php
+++ app/Models/SpecialRequest.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class SpecialRequest extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'request_type',
+        'request_details',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function allergies()
+    {
+        return $this->belongsToMany(Allergy::class, 'request_allergies');
+    }
 }
