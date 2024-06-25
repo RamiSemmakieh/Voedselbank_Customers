3fdf2ed43ed2f39955e8dc2b67683ee53dfea76a
ramos013 <334020@student.mboutrecht.nl>
2024-06-25 14:30:43 +0200

    Editing styling

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -1,4 +1,3 @@
-<!-- resources/views/customers/create.blade.php -->
 <x-app-layout>
     <div class="mt-16 max-w-2xl mx-auto p-6 bg-gray-800 rounded-lg shadow-md">
         <h1 class="text-2xl font-bold text-white mb-4">Create Customer</h1>
--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,4 +1,3 @@
-<!-- resources/views/customers/index.blade.php -->
 <x-app-layout>
     <div class="mt-16 max-w-6xl mx-auto p-6 bg-gray-800 rounded-lg shadow-md">
         <h1 class="text-2xl font-bold text-white mb-4">Customers List</h1>
