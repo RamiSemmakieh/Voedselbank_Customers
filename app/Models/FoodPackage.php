d39023fd8a39b067782cb55ea32ac2f268a9a100
ramos013 <334020@student.mboutrecht.nl>
2024-06-25 10:47:34 +0200

    Add foodpacage model with attributes and relationships

--- app/Models/FoodPackage.php
+++ app/Models/FoodPackage.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class FoodPackage extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'package_number',
+        'customer_id',
+        'creation_date',
+        'distribution_date',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function packageItems()
+    {
+        return $this->hasMany(PackageItem::class);
+    }
 }
