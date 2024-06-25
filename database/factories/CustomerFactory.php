ef5fab9e65328a3c8e381403596e73cf092b1d9b
ramos013 <334020@student.mboutrecht.nl>
2024-06-25 13:07:56 +0200

    Add CustomerFactory to generate fake data for Customer model

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -2,22 +2,26 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
- */
 class CustomerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    protected $model = Customer::class;
+
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' => $this->faker->phoneNumber,
+            'number_of_adults' => $this->faker->numberBetween(1, 5),
+            'number_of_children' => $this->faker->numberBetween(0, 5),
+            'number_of_babies' => $this->faker->numberBetween(0, 3),
+            'street_name' => $this->faker->streetName,
+            'house_number' => $this->faker->buildingNumber,
+            'postal_code' => $this->faker->postcode,
+            'city' => $this->faker->city,
         ];
     }
 }
